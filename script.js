2dc49fc5c9ddd3217d10c1cc2b14946c0571525c
0315311001-ops <0315311001@s.tusd1.org>
2026-04-30 16:24:22 +0000

    day 2

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 console.log("Javascript file loaded")
 
+displayQuestion();
+let something = [
+    {
+        question: "Whos your Favorite Female Artist?",
+        option1: "Ariana Grande",
+        option2: "Billie Eilish",
+        option3: "Doja Cat",
+        option4: "Melanie Martinez",
+        option5: "Tate McRae",
+        option6: "SZA",
 
+
+    },
+    {
+        question: "Whos your Favorite Male Artist?",
+        option1: "Michael Jackaon",
+        option2: "Justin Bieber",
+        option3: "Troye Sivan",
+        option4: "The Weeknd",
+        option5: "Harry Styles",
+        option6: "Bad Bunny",
+    }
+];
